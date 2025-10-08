5b30e977e72e56016939f370fd8e9674d59bbc47
Yvonne Tashy <tashyvie@gmail.com>
2025-10-08 20:34:11 +0300

    Add files via upload

---
+++ variables.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int a=3;
+	float b=5.1f;
+	double c=4.9;
+	string name ="Tashy";
+	cout<<"Hi"<< name<<"!\n";
+	cout<<"You have a "<<c<< "GPA\n";
+	cout<<"You made it to the top "<<b<<"%\n";
+	cout<<"You are number "<< a;
+}
